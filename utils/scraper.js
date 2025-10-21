74dd69b6ec8477a0e394bf8030524340cf5bb2bd
Codingapo <124777804+Codingapo@users.noreply.github.com>
2025-10-21 12:35:18 +0200

    Create scraper.js

---
+++ utils/scraper.js
@@ -0,0 +1,26 @@
+import { load } from 'cheerio';
+import axios from 'axios';
+
+export const extractEpisodes = (html) => {
+  const $ = load(html);
+  const response = [];
+
+  $('.ssl-item.ep-item').each((i, el) => {
+    const obj = {
+      episodeNumber: i + 1,
+      title: $(el).attr('title'),
+      alternativeTitle: $(el).find('.ep-name.e-dynamic-name').attr('data-jname'),
+      id: $(el).attr('href').replace('/watch/', '').replace('?', '::'),
+      isFiller: $(el).hasClass('ssl-item-filler'),
+    };
+    response.push(obj);
+  });
+
+  return response;
+};
+
+export const fetchAnimeHTML = async (slug) => {
+  const url = `https://hianime.to/anime/${slug}`;
+  const { data } = await axios.get(url);
+  return data;
+};
